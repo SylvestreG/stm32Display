6b55141a0b63272b88123088c9247b871dabdd16
Sylvestre Gallon <ccna.syl@gmail.com>
2017-05-15 23:56:15 +0200

    here a working led flashing binary

---
+++ stm32.cmake
@@ -0,0 +1,20 @@
+set(CMAKE_SYSTEM_NAME Generic)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+include(CMakeForceCompiler)
+
+set(CMAKE_TRY_COMPILE_TARGET_TYPE STATIC_LIBRARY)
+
+set(CMAKE_C_COMPILER arm-none-eabi-gcc)
+set(CMAKE_CXX_COMPILER arm-none-eabi-g++)
+set(CMAKE_ASM_COMPILER arm-none-eabi-gcc)
+
+set(CMAKE_OBJCOPY arm-none-eabi-objcopy)
+set(CMAKE_OBJDUMP arm-none-eabi-objdump)
+set(CMAKE_SIZE arm-none-eabi-size)
+set(CMAKE_DEBUGER arm-none-eabi-gdb)
+set(CMAKE_CPPFILT arm-none-eabi-c++filt)
+
+set(CMAKE_C_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m3 -Wall -Wextra -Werror -std=c99 -nostdinc")
+set(CMAKE_CXX_FLAGS "-mthumb -fno-builtin -mcpu=cortex-m3 -Wall -Wextra -Werror -std=c++11 -nostdinc")
+set(CMAKE_ASM_FLAGS "-mthumb -fno-builtin -x assembler-with-cpp")
+set(CMAKE_EXE_LINKER_FLAGS "-Wl,--gc-sections -mthumb -mcpu=cortex-m3 -mabi=aapcs -nostdlib")
